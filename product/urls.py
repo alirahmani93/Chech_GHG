7ebdd39a8e6e88796203dcf0b444fb9e4a52fd3a
Ali_Rahmani <ali93rahmani@gmail.com>
2021-09-08 17:12:34 +0430

    comment product template part

--- product/urls.py
+++ product/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('show_all_c/', show_all_category, name="show_all_category"),
     path('show_all_m/', show_all_media, name="show_all_media"),
     path('selcted_p/<int:id>', selected_product, name="selected_product"),
-    path('show_price/<str:upc>', show_price, name="show_price"),
 ]
 
 # path('show_all_p/<str:cat>', show_all_product, name="show_all_product"),
