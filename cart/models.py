5c639d9c48cb3dc5d60877067f89a3ec0c07b17e
Ali_Rahmani <ali93rahmani@gmail.com>
2021-10-03 17:46:02 +0330

    signal views

--- cart/models.py
+++ cart/models.py
@@ -22,7 +22,7 @@ class Cart(Log):
     status_choices = (('on_cart', 'on_cart'), ('ready_to_pay', 'ready_to_pay'))
     # FK
     user_fk = models.ForeignKey(OurUser, on_delete=models.RESTRICT, null=True, blank=True)
-    cart_item = models.ForeignKey("CartItem", on_delete=models.RESTRICT, null=True, blank=True)
+    # cart_item = models.ForeignKey("CartItem", on_delete=models.RESTRICT, null=True, blank=True)
     # Attributes
     # cart_uuid = models.UUIDField(verbose_name="شماره کارت ساخته شده", unique_for_date=1, default=None,)
     cart_status = models.CharField(max_length=100, choices=status_choices, default=status_choices[0])
@@ -38,26 +38,28 @@ class Cart(Log):
 
 
 class CartItem(Log):
+    status_choices = (('pending', 'pending'), ('paid', 'paid'), ('None', 'None'))
+
     # FK
-    product_fk = models.ForeignKey(Product, on_delete=models.RESTRICT, null=True, blank=True)
+    cart_fk = models.ForeignKey("Cart", on_delete=models.RESTRICT,related_name="cart_fk", null=True, blank=True)
+    product_fk = models.ForeignKey(Product, on_delete=models.RESTRICT,related_name="product_fk", null=True, blank=True)
     # Attrs
     quantity = models.IntegerField(default=0)
-    active = models.BooleanField(default=True)
+    status = models.BooleanField(default=True)
 
     @property
     def cond1(self) -> int:
         print("up")
-        x= Product.objects.get(id=self.product_fk.id).count
-        print(x,self.quantity)
+        x = Product.objects.get(id=self.product_fk.id).count
+        print(x, self.quantity)
 
         if x - self.quantity > 0:
-            y= x- self.quantity
-            Product.objects.get(id=self.product_fk.id).count=y
+            y = x - self.quantity
+            y = Product.objects.get(id=self.product_fk.id).count
             print(y)
             return True
         else:
             return False
-
 
     # def save(self, **kwargs):
     #     print("SAVE")
